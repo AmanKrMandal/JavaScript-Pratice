981f3b6a9ace051b39b3c36fa7b743d1e756ef8e
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-08-30 13:26:32 +0530

    Create this keyword.js

---
+++ this keyword.js
@@ -0,0 +1,13 @@
+       let person = {
+            fristName :'aman',
+            lastName :'mandal',
+            sayHello(){
+                console.log('hey  ' + this.fristName  + " "+car.brand )
+            }
+        }
+        let car={
+            brand : 'wwww',
+            model : 'style',
+        }
+
+        person.sayHello()
