a9a67cf3467f378ae438c1d74b011efe15b1fb9a
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-08-31 09:43:38 +0530

    Update Async Await.js

--- Async Await.js
+++ Async Await.js
@@ -1,4 +1,19 @@
+//---------example 1-----------------
 
+
+async function test(){
+    console.log("2: Message");
+    const response = await fetch("sample.json")
+    console.log("3: Message"); 
+    const students = await response.json();
+    return students;
+    }
+    console.log("1 : Message");
+    let a = test();
+    console.log("4: Message");
+    console.log(a);
+
+--------------------------
 async function test(){
     console.log('a')
    await console.log('b')
